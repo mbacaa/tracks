b61d84612a58bea3ed329c358e4a4b230d918b7b
Miłosz Baca <m.baca.460@studms.ug.edu.pl>
2023-12-02 21:02:46 +0100

    feat: added not-found page

---
+++ src/app/(studio)/layout.tsx
@@ -0,0 +1,19 @@
+import Footer from '@/components/Footer'
+import Header from '@/components/Header'
+import { getUser } from '@/server/auth'
+
+interface LandingLayoutProps {
+	children: React.ReactNode
+}
+
+export default async function LandingLayout({ children }: LandingLayoutProps) {
+	const user = await getUser()
+
+	return (
+		<div className='min-h-screen flex flex-col'>
+			<Header user={user} />
+			<main className='flex-1'>{children}</main>
+			<Footer />
+		</div>
+	)
+}
